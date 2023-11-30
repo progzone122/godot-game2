d954be1ad3ca5417e6f230d11b7cf73a576fbe32
progzone122 <80642969+progzone122@users.noreply.github.com>
2023-11-30 12:54:01 +0200

    .

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 @onready var coins = $"CanvasLayer/VBoxContainer/HBoxContainer2/coin"
+@onready var label = $"CanvasLayer/VBoxContainer/HBoxContainer/Label"
 
 func _ready():
+	if Global.hp != 0:
+		label.text = "VICTORY"
+	else:
+		label.text = "DEFEAT"
 	coins.text = str(Global.coins)
